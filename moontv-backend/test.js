ff475e0eaf5ccd16a25bf9bf8dd00d86679be75c
francoilleslb-cmyk <francoilleslb@gmail.com>
2026-03-03 09:04:22 -0300

    Update test.js

--- moontv-backend/test.js
+++ moontv-backend/test.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const runScraper = require('./runEventosScraper');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI) || 'tu_url_de_mongo')
+mongoose.connect(process.env.MONGODB_URI)
   .then(async () => {
     console.log("Conectado. Empezando...");
     await runScraper();
